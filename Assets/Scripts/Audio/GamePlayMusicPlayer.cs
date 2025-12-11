bca50377e5304163e9d4a709be4503b18b8ebfe5
yzhang887 <y.zhang887@outlook.com>
2025-12-11 20:19:57 +1100

    Update GamePlayMusicPlayer.cs

--- Assets/Scripts/Audio/GamePlayMusicPlayer.cs
+++ Assets/Scripts/Audio/GamePlayMusicPlayer.cs
@@ -1,1 +1,40 @@
+using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+public class GameplayMusicPlayer: MonoBehaviour
+{
+    [SerializeField] private AudioClip gameplayMusic;
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        // 2D audio
+        _audioSource.spatialBlend = 0f;
+
+        // playloop
+        _audioSource.loop = true;
+
+        // playonawake
+        _audioSource.playOnAwake = false;
+    }
+
+    private void Start()
+    {
+        if (gameplayMusic != null)
+        {
+            _audioSource.clip = gameplayMusic;
+
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
+        }
+        else
+        {
+            Debug.LogWarning("GameplayMusicPlayer: No gameplayMusic assigned!");
+        }
+    }
+}
