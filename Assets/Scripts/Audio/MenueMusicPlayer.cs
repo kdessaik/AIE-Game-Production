c73318e5cc5d563872820170978a0702059b0d23
yzhang887 <y.zhang887@outlook.com>
2025-12-11 19:49:57 +1100

    Updating MenuMusicPlayer.cs

--- Assets/Scripts/Audio/MenueMusicPlayer.cs
+++ Assets/Scripts/Audio/MenueMusicPlayer.cs
@@ -1,1 +1,27 @@
+using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+public class MenuMusicPlayer: MonoBehaviour
+{
+    [SerializeField] private AudioClip menuMusic;
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.spatialBlend = 0f;
+        _audioSource.loop = true;
+        _audioSource.playOnAwake = false;
+    }
+
+    private void Start()
+    {
+        if (menuMusic != null)
+        {
+            _audioSource.clip = menuMusic;
+
+            if (!_audioSource.isPlaying)
+                _audioSource.Play();
+        }
+    }
+}
